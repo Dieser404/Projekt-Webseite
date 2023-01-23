893e0d6e92ef519ffe0d1ae2b4bcb2ada10f0ec3
T <tknaupp@outlook.de>
2023-01-23 20:36:06 +0100

    first commit

---
+++ js/easylib.js
@@ -0,0 +1,47 @@
+function includeHTML() {
+  var z, i, elmnt, file, xhttp,urlpara="";
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain attribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if ( file === "url" ) {
+      if (window.location.search == "" ) {
+        file = "components/Startseite.html"
+      } else 
+      {
+        urlpara = getParaByName(file, url = window.location.href);
+        console.log(urlpara);
+        file = urlpara;
+      }
+    }
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      } 
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+};
+
+function getParaByName(name, url = window.location.href) {
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
